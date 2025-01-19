695945ff4065d7dc63d7fedeeddc52062add170d
Raj bharati <rajbharati@dsmartsys.in>
2025-01-19 13:28:37 -0600

    feat: Add colour to console

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -5,26 +5,41 @@ import config from '../config/config';
 import { EApplicationEnvironment } from '../constant/application';
 import path from 'path';
 import * as sourceMapSupport from 'source-map-support';
+import { red, blue, yellow, green, magenta } from 'colorette';
 
 //linking trace support
 sourceMapSupport.install();
 
+const colorizeLevel = (level: string) => {
+    switch (level) {
+        case 'ERROR':
+            return red(level);
+        case 'INFO':
+            return blue(level);
+        case 'WARN':
+            return yellow(level);
+        default:
+            return level;
+    }
+};
+
 const consoleLogFormat = format.printf((info) => {
     const { level, message, timestamp, meta = {} } = info;
 
-    const customLevel = level.toUpperCase();
+    const customLevel = colorizeLevel(level.toUpperCase());
 
-    const customTimeStamp = timestamp;
+    const customTimeStamp = green(timestamp as string);
 
     const customMessage = message;
 
     const customMeta = util.inspect(meta, {
         showHidden: false,
-        depth: null
+        depth: null,
+        colors: true
     });
 
     // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-    const customLog = ` ${customLevel} [${customTimeStamp}] ${customMessage}\n${'META'} ${customMeta}`;
+    const customLog = ` ${customLevel} [${customTimeStamp}] ${customMessage}\n${magenta('META')} ${customMeta}`;
 
     return customLog;
 });
