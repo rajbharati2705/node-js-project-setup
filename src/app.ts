fa313f7cbc9f1fa57e377b25b6de93ca3942799a
Raj bharati <rajbharati@dsmartsys.in>
2025-01-19 11:45:10 -0600

    feat: Add 404 not found

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
-import express, { Application } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 import path from 'path';
 import router from './router/apiRouter';
 import globalErrorHandler from './middleware/globalErrorHandler';
+import responseMessage from './constant/responseMessage';
+import httpError from './util/httpError';
 
 const app: Application = express();
 
@@ -11,6 +13,15 @@ app.use(express.static(path.join(__dirname, '../', 'public')));
 
 //routes
 app.use('/api/v1', router);
+
+//404 not found
+app.use((req: Request, _: Response, next: NextFunction) => {
+    try {
+        throw new Error(responseMessage.NOT_FOUND('route'));
+    } catch (error) {
+        httpError(next, error, req, 404);
+    }
+});
 
 //global error handler
 app.use(globalErrorHandler);
--- src/constant/responseMessage.ts
+++ src/constant/responseMessage.ts
@@ -1,4 +1,5 @@
 export default {
     SUCCESS: 'success',
-    SOMETHING_WENT_WRONG: 'something went wrong'
+    SOMETHING_WENT_WRONG: 'something went wrong',
+    NOT_FOUND: (entity: string) => `${entity} not found`
 };
